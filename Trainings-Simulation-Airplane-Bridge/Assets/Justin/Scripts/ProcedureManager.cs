7bdeba0be86e074b8702b6f00f4a8cf1fd1fe3cb
justifrusti <70446225+justifrusti@users.noreply.github.com>
2022-05-17 11:44:49 +0200

    Started Working on Procedure manager

---
+++ Trainings-Simulation-Airplane-Bridge/Assets/Justin/Scripts/ProcedureManager.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProcedureManager : MonoBehaviour
+{
+    public enum ProcedureState { ReleasingBridge, StartUp, OpenShutters, Moving, WheelPosition, PositioningBridge }
+    public enum StartupState { TestingLights, StartingUp}
+    [Header("Procedure State")]
+    public ProcedureState procedureState;
+    public StartupState startupState;   
+
+    //Header For Releasing Components;
+
+    [Header("Startup Components")]
+    public bool testedLight = false;
+    public bool startIsActive = false;
+
+    [Header("Open Shutters Components")]
+    public bool shuttersOpened = false;
+
+    [Header("Moving Components")]
+    public bool test;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(procedureState)
+        {
+            case ProcedureState.ReleasingBridge:
+                //Insert Visual Elements Code here;
+
+                procedureState = ProcedureState.StartUp;
+                break;
+
+            case ProcedureState.StartUp:
+                switch(startupState)
+                {
+                    case StartupState.TestingLights:
+                        if(!testedLight)
+                        {
+                            Debug.Log("Lights not Tested");
+                        }else if(testedLight)
+                        {
+                            startupState = StartupState.StartingUp;
+                        }
+                        break;
+
+                    case StartupState.StartingUp:
+                        if(!startIsActive)
+                        {
+                            Debug.Log("Hand button not Pressed");
+                        }
+                        else if(startIsActive)
+                        {
+                            procedureState = ProcedureState.OpenShutters;
+                        }
+                        break;
+                }
+                break;
+
+            case ProcedureState.OpenShutters:
+                break;
+
+            case ProcedureState.Moving:
+                break;
+
+            case ProcedureState.WheelPosition:
+                break;
+
+            case ProcedureState.PositioningBridge:
+                break;
+        }
+    }
+
+    public void PressedLightTestButton()
+    {
+        testedLight = true;
+    }
+
+    public void PressedStartupButton()
+    {
+        startIsActive = true;  
+    }
+}
