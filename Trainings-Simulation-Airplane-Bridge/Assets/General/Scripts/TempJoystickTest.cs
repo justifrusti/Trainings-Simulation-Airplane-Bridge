b9a85c0e9291d0889656a8d4acde0fd8b211cb22
snoepkanon <78019309+snoepkanon@users.noreply.github.com>
2022-05-24 16:10:34 +0200

    rotation clamps but no

--- Trainings-Simulation-Airplane-Bridge/Assets/General/Scripts/TempJoystickTest.cs
+++ Trainings-Simulation-Airplane-Bridge/Assets/General/Scripts/TempJoystickTest.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class TempJoystickTest : MonoBehaviour
 {
     public Transform topOfJoystick;
-    public Collider topColl;
 
-    [SerializeField]
     private float forwardBackwardTilt = 0;
-    [SerializeField]
     private float sideToSideTilt = 0;
+
+
+    private float rot = 0f;
+    private float rotX;
+    public float minRotX;
+    public float maxRotX;
+    
+    private float rotSpeed = 3f;
+
+    public Vector3 rotation;
+
+    public Transform rotPoint;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,12 +33,10 @@ public class TempJoystickTest : MonoBehaviour
         if (forwardBackwardTilt < 355 && forwardBackwardTilt > 290)
         {
             forwardBackwardTilt = Mathf.Abs(forwardBackwardTilt - 360);
-            Debug.Log("backward" + forwardBackwardTilt);
             //bridgeController.bridgeState = BridgeController.BridgeState.Backward;
         }
         else if (forwardBackwardTilt > 5 && forwardBackwardTilt < 74)
         {
-            Debug.Log("forward" + forwardBackwardTilt);
             //bridgeController.bridgeState = BridgeController.BridgeState.Forward;
         }
 
@@ -37,20 +44,29 @@ public class TempJoystickTest : MonoBehaviour
         if (forwardBackwardTilt < 355 && forwardBackwardTilt > 290)
         {
             sideToSideTilt = Mathf.Abs(sideToSideTilt - 360);
-            Debug.Log("Right" + sideToSideTilt);
             //bridgeController.bridgeState = BridgeController.BridgeState.Right;
         }
         else if (sideToSideTilt > 5 && sideToSideTilt < 74)
         {
-            Debug.Log("Left" + sideToSideTilt);
             //bridgeController.bridgeState = BridgeController.BridgeState.Left;
         }
+        lockedRotation();
+        //transform.Rotate(rotation * Time.deltaTime); 
     }
+
     public void OnTriggerStay(Collider other)
     {
         if (other.tag == "PlayerHand")
         {
             transform.LookAt(other.transform.position, transform.up);
         }
+    }
+
+    void lockedRotation()
+    {
+        rotX += Input.GetAxis("Mouse X");
+        rotX = Mathf.Clamp(rotX, minRotX, maxRotX);
+        //rotY += Input.GetAxis("Horizontal" * Time.deltaTime;
+        //transform.rotation = Quaternion.Euler(rotX, rotationY, 0);
     }
 }
