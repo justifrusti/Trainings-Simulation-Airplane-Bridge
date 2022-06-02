1c7e26fe55b972ba91b3a06da41481a971fe053c
snoepkanon <78019309+snoepkanon@users.noreply.github.com>
2022-06-02 16:41:36 +0200

    input voor joystick + fix voor joystick

--- Trainings-Simulation-Airplane-Bridge/Assets/General/Scripts/TempJoystickTest.cs
+++ Trainings-Simulation-Airplane-Bridge/Assets/General/Scripts/TempJoystickTest.cs
@@ -1,14 +1,17 @@
- using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
+using System;
+using Valve.VR;
 using UnityEngine;
 
 public class TempJoystickTest : MonoBehaviour
 {
     public Transform topOfJoystick;
 
+    [SerializeField]
     private float forwardBackwardTilt = 0;
+    [SerializeField]
     private float sideToSideTilt = 0;
-
 
     private float rot = 0f;
     private float rotX;
@@ -17,9 +20,7 @@ public class TempJoystickTest : MonoBehaviour
     
     private float rotSpeed = 3f;
 
-    public Vector3 rotation;
-
-    public Transform rotPoint;
+    public SteamVR_Action_Single squezeAction;
     // Start is called before the first frame update
     void Start()
     {
@@ -50,18 +51,30 @@ public class TempJoystickTest : MonoBehaviour
         {
             //bridgeController.bridgeState = BridgeController.BridgeState.Left;
         }
-        lockedRotation();
-        //transform.Rotate(rotation * Time.deltaTime); 
-    }
-
-    public void OnTriggerStay(Collider other)
-    {
-        if (other.tag == "PlayerHand")
+        //lockedRotation();
+        
+        if (SteamVR_Actions._default.GrabGrip.GetStateDown(SteamVR_Input_Sources.Any))
         {
-            transform.LookAt(other.transform.position, transform.up);
+            print("test");
         }
     }
 
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("PlayerHand"))
+        {
+            if (SteamVR_Actions._default.GrabPinch.GetState(SteamVR_Input_Sources.Any))
+            {
+                if (SteamVR_Actions._default.GrabGrip.GetState(SteamVR_Input_Sources.Any))
+                {
+                    transform.LookAt(other.transform.position, transform.up);
+                }
+            }
+        }
+        
+        
+    }
+    
     void lockedRotation()
     {
         rotX += Input.GetAxis("Mouse X");
