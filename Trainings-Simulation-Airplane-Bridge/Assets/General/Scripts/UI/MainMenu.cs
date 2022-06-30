013450153dff1d6c4c31891e5a1d89b6619ff1fc
Ghostrifter <60340180+Ghostrifter@users.noreply.github.com>
2022-06-30 12:19:07 +0200

    small edit

--- Trainings-Simulation-Airplane-Bridge/Assets/General/Scripts/UI/MainMenu.cs
+++ Trainings-Simulation-Airplane-Bridge/Assets/General/Scripts/UI/MainMenu.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     public void StartGame()
     {
-        print("Pressed Start");
+        SceneManager.LoadScene(1);
     }
 
     public void QuitGame()
