759ece8f7a95beb6517160597ce597aa09ec0a2c
Ghostrifter <60340180+Ghostrifter@users.noreply.github.com>
2022-07-01 14:12:21 +0200

    Added Components To UI Buttons + ToMainMenu function in UI Script

--- Trainings-Simulation-Airplane-Bridge/Assets/General/Scripts/UI/MainMenu.cs
+++ Trainings-Simulation-Airplane-Bridge/Assets/General/Scripts/UI/MainMenu.cs
@@ -10,6 +10,11 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(1);
     }
 
+    public void ToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
     public void QuitGame()
     {
         Application.Quit();
